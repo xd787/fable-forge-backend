490383f2294d6706dd7657ac35ec0154b6689379
Alienor <alienordms@gmail.com>
2023-10-23 10:08:52 +0200

    route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const sotriesRouter = require ('./routes/stories')
 
 var app = express();
 const cors = require('cors');
@@ -22,5 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stories', sotriesRouter)
 
 module.exports = app;
