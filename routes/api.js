c2335d241863e351731d46b64a88ebe6a606271d
Alienor <alienordms@gmail.com>
2023-10-25 15:14:56 +0200

    don

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 
-// Backend endpoint pour générer l'histoire
+// Backend endpoint for generating the story
 router.post("/generate-story", async (req, res) => {
   const { genre, fin, longueur } = req.body;
 
@@ -17,72 +17,57 @@ router.post("/generate-story", async (req, res) => {
   } else if (longueur === "3") {
     maxTokens = Math.floor(Math.random() * (4000 - 1500 + 1) + 1500);
   } else {
-    // Valeur par défaut si la sélection n'est pas valide
+    // Default value if the selection is not valid
     maxTokens = 1000;
   }
 
   try {
-    // Construisez le message utilisateur à partir des données reçues
-    const userMessage = `Je souhaite créer une histoire de genre ${genre} d'environ ${longueur} pages, soit environ 300 tokens par page A4. Assurez-vous que l'histoire a une fin ${fin} en accord avec le genre. M'inspirer pour le personnage principal, le lieu de départ et l'époque. Créer aussi un titre avant le texte de l'histoire. `;
+    // Construct the user message from the received data
+    const userMessage = `Je souhaite créer une histoire de genre ${genre} d'environ ${longueur} pages, soit environ 300 tokens par page A4. Assurez-vous que l'histoire a une fin ${fin} en accord avec le genre. M'inspirer pour le personnage principal, le lieu de départ et l'époque. Créer aussi un titre avant le texte de l'histoire.`;
 
-    // Utilisez ces informations pour créer la requête à l'API OpenAI
+    // Use this information to create the request to the OpenAI API
     const requestBody = {
       model: "gpt-3.5-turbo-16k",
       messages: [
         { role: "system", content: "You are the best storyteller there is." },
-        { role: "user", content: userMessage},
+        { role: "user", content: userMessage },
       ],
-      max_tokens: maxTokens, // Define maxTokens based on longueur
+      max_tokens: maxTokens,
       temperature: 1,
     };
 
-    const storyResponse = await fetch(
-      `https://api.openai.com/v1/chat/completions`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${API_KEY}`,
-        },
-        body: JSON.stringify(requestBody),
-      }
-    );
+    const storyResponse = await fetch(`https://api.openai.com/v1/chat/completions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify(requestBody),
+    });
 
     if (!storyResponse.ok) {
-      throw new Error("Erreur lors de la génération de l'histoire reponse false");
+      throw new Error("Error generating the story");
     }
-
-
-
 
     const storyData = await storyResponse.json();
     const storyMessage = storyData.choices[0].message.content;
-    
 
-    // Extract the title from the story text
-    // const titleMatch = storyMessage.match(/Titre\s*:\s*"(.*?)"/);
-    // const titleMatch = storyMessage.match(/Title:\s*"(.*?)"/);
+    // Find the index of the first occurrence of a newline character
+    const newLineIndex = storyMessage.indexOf("\n");
 
-  //   const titleRegex = /Titre\s*:\s*"([^"]+)/;
-  //   const titleMatch = storyMessage.match(titleRegex);
-
-  //   if (titleMatch && titleMatch[1]) {
-  //    const title = titleMatch[1];
-  //  // Remove the title from the story content
-  //   const storyWithoutTitle = storyMessage.replace(titleMatch[0], '').trim();   
-     res.json({storyMessage});
-    // } else {
-    //   console.log('Title not found. Input Text:', storyMessage);
-    // }
-
-
-
+    if (newLineIndex !== -1) {
+      const title = storyMessage.slice(0, newLineIndex).trim();
+      const storyWithoutTitle = storyMessage.slice(newLineIndex).trim();
+      res.json({ title, storyWithoutTitle });
+    } else {
+      // Handle the case where no newline character is found
+      res.json({ title: "Title not found", storyMessage });
+    }
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ error: "Erreur lors de la génération de l'histoire etape 1" });
+    res.status(500).json({ error: "Error generating the story" });
   }
 });
 
 module.exports = router;
+
