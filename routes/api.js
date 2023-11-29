4ac7b6889b2d0e6fce0fe6562541612a254d5043
Alienor <alienordms@gmail.com>
2023-11-29 11:25:51 +0100

    api in the back

--- routes/api.js
+++ routes/api.js
@@ -1,45 +1,37 @@
-// file that calls the API from the backend
-// Not used for the moment, to be kept for future development of the mobile application.
-
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
 const API_KEY = process.env.API_KEY;
-const API_URL = process.env.API_URL
-
-const MAX_TOKENS = {
-  Courte: { min: 400, max: 800 },
-  Moyenne: { min: 800, max: 1200 },
-  Longue: { min: 1200, max: 1600 },
-};
+const API_URL = process.env.API_URL;
 
 // POST: Generate a story with all settings (type, style, length)
 router.post("/generate-story", async (req, res) => {
-  const { genre, fin, longueur } = req.body;
+  const { type, endingType, length } = req.body;
 
-  // USER MESSAGE
-  const prompt = `Je souhaite créer une histoire de genre ${genre} de longueur ${longueur}. Je veux une ${fin}.`;
+  // Longueur d'histoire possible (en token)
+  const LENGTH_MAP = {
+    Courte: { min: 800, max: 1300 },
+    Moyenne: { min: 1500, max: 2000 },
+    Longue: { min: 2000, max: 2500 },
+  };
+  const tokenCount =
+    Math.floor(
+      Math.random() * (LENGTH_MAP[length].max - LENGTH_MAP[length].min + 1)
+    ) + LENGTH_MAP[length].min;
 
-  // LENGTH GENERATOR
-  const lengthKey = ["Courte", "Moyenne", "Longue"].includes(longueur)
-    ? longueur
-    : "Courte";
-  const { min, max } = MAX_TOKENS[lengthKey];
-  const maxTokens = Math.floor(Math.random() * (max - min + 1) + min);
+  const prompt = `Je souhaite créer une histoire de genre ${type}. Je veux une ${endingType}.`;
 
+  // Initialize variables
+  let totalTokens = 0;
+  let generatedStory = "";
 
+  // LOOP TOKEN
+  while (totalTokens < tokenCount) {
+    const remainingTokens = tokenCount - totalTokens;
+    const maxTokensForChunk = remainingTokens > 250 ? 250 : remainingTokens;
 
-  const generateNextChunk = async () => {
-    let totalTokens = 0;
-    const storyChunks = [];
-
-    while (totalTokens < maxTokens) {
-      // Generate a chunk with a maximum of 250 tokens
-      const remainingTokens = maxTokens - totalTokens;
-      const tokensInThisChunk = remainingTokens > 250 ? 250 : remainingTokens;
-
-    // Préparation de la requête à envoyer à l'API
+    //MESSAGE BODY API
     const data = {
       model: "gpt-3.5-turbo-16k",
       messages: [
@@ -47,80 +39,74 @@ router.post("/generate-story", async (req, res) => {
           role: "system",
           content: `
           Tu es un conteur d'histoires français, avec les consignes suivantes :\n\n-
-          Tu vas créer une seule et unique histoire.\n-
+          Tu vas créer une seule et unique histoire avec une fin qui ne dépassera pas le nombre maximum de tokens.\n-
           Tu ne commenceras pas les histoires par \"il était une fois\".\n- 
-          Créer aussi un titre avant le texte de l'histoire que tu mettras entre des balises \"!\".\n-`
+          Créer aussi un titre avant le texte de l'histoire que tu mettras entre des balises \"!\".\n-`,
         },
-        { 
+        {
           role: "user",
-          content: prompt 
-        }, // Message de l'utilisateur actuel
+          content: prompt,
+        },
       ],
-  
-      // Contrôle du style et de la diversité de la génération de texte
       temperature: 1,
-      max_tokens: 250,
+      max_tokens: maxTokensForChunk,
       top_p: 1,
       frequency_penalty: 1,
       presence_penalty: 1,
     };
-  
 
-      try {
-        // Fetch content for this chunk from the API
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    try {
+      // FETCH API
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${API_KEY}`,
           },
           body: JSON.stringify(data),
-        });
+        }
+      );
 
-        // Process the response and extract content for this chunk
-        const responseData = await response.json();
-        const generatedContent = responseData.generatedContent; // Adjust based on API response structure
-
-        // Extraction of the title and response content
-        const titleRegex = /!(.*?)!/;
-        const titleMatch = titleRegex.exec(generatedContent);
-        const title = titleMatch ? titleMatch[1] : "";
-
-        // Separating title from the main content
-        const contentWithoutTitle = generatedContent.replace(titleRegex, "");
-
-        // Constructing the chunk with title and response content
-        const chunk = {
-          title,
-          content: contentWithoutTitle,
-        };
-
-        // Push the generated chunk to the storyChunks array
-        storyChunks.push(chunk);
-
-        // Update totalTokens with tokensInThisChunk
-        totalTokens += tokensInThisChunk;
-      } catch (error) {
-        // Handle errors from API fetch
-        throw new Error("Error fetching story content");
+      //REPONSE API
+      const responseData = await response.json();
+      if (!response.ok || !responseData.choices || !responseData.choices[0]) {
+        res.json({ result: "Erreur lors de la génération de l'histoire" });
       }
+      const generatedContent = responseData.choices[0].message.content.trim();
+
+      //TOTAL TOKEN COUNT
+      totalTokens += generatedContent.length;
+      generatedStory += generatedContent;
+
+      // Extraction of the title and response content
+      const titleRegex = /!(.*?)!/;
+      const titleMatch = titleRegex.exec(generatedStory);
+      const title = titleMatch ? titleMatch[1] : "";
+      const contentWithoutTitle = generatedStory.replace(titleRegex, "");
+
+      //BREAK WHEN DONE 
+      if (totalTokens >= tokenCount) {
+        
+        console.log(contentWithoutTitle.length)
+        return res.json({
+          result: "Story generated successfully",
+          title,
+          contentWithoutTitle,
+        });
+       
+      }
+
+
+      //CATCH ERROR
+    } catch (error) {
+      // Handle errors during API call
+      return res
+        .status(500)
+        .json({ error: "Error generating story", details: error.message });
     }
-
-    // Return the generated story chunks
-    return storyChunks;
-  };
-
-  try {
-    // Generate the story chunks
-    const story = await generateNextChunk();
-
-    // Respond with the generated story chunks or handle as needed
-    res.json({ result: "Story generated successfully", story });
-  } catch (error) {
-    // Handle errors if any
-    res.status(500).json({ result: "Error generating story", error: error.message });
   }
 });
 
 module.exports = router;
-
---
+++ routes/old.js
@@ -0,0 +1,123 @@
+// // file that calls the API from the backend
+// // Not used for the moment, to be kept for future development of the mobile application.
+
+// const express = require("express");
+// const router = express.Router();
+// const fetch = require("node-fetch");
+
+// const API_KEY = process.env.API_KEY;
+// const API_URL = process.env.API_URL
+
+// const MAX_TOKENS = {
+//   Courte: { min: 400, max: 800 },
+//   Moyenne: { min: 800, max: 1200 },
+//   Longue: { min: 1200, max: 1600 },
+// };
+
+// // POST: Generate a story with all settings (type, style, length)
+// router.post("/generate-story", async (req, res) => {
+//   const { genre, fin, longueur } = req.body;
+
+//   // USER MESSAGE
+//   const prompt = `Je souhaite créer une histoire de genre ${genre} de longueur ${longueur}. Je veux une ${fin}.`;
+
+//   // LENGTH GENERATOR
+//   const lengthKey = ["Courte", "Moyenne", "Longue"].includes(longueur)
+//     ? longueur
+//     : "Courte";
+//   const { min, max } = MAX_TOKENS[lengthKey];
+//   const maxTokens = Math.floor(Math.random() * (max - min + 1) + min);
+
+
+
+//   const generateNextChunk = async () => {
+//     let totalTokens = 0;
+//     const storyChunks = [];
+
+//     while (totalTokens < maxTokens) {
+//       // Generate a chunk with a maximum of 250 tokens
+//       const remainingTokens = maxTokens - totalTokens;
+//       const tokensInThisChunk = remainingTokens > 250 ? 250 : remainingTokens;
+
+//     // Préparation de la requête à envoyer à l'API
+//     const data = {
+//       model: "gpt-3.5-turbo-16k",
+//       messages: [
+//         {
+//           role: "system",
+//           content: `
+//           Tu es un conteur d'histoires français, avec les consignes suivantes :\n\n-
+//           Tu vas créer une seule et unique histoire.\n-
+//           Tu ne commenceras pas les histoires par \"il était une fois\".\n- 
+//           Créer aussi un titre avant le texte de l'histoire que tu mettras entre des balises \"!\".\n-`
+//         },
+//         { 
+//           role: "user",
+//           content: prompt 
+//         }, // Message de l'utilisateur actuel
+//       ],
+  
+//       // Contrôle du style et de la diversité de la génération de texte
+//       temperature: 1,
+//       max_tokens: 250,
+//       top_p: 1,
+//       frequency_penalty: 1,
+//       presence_penalty: 1,
+//     };
+  
+
+//       try {
+//         // Fetch content for this chunk from the API
+//         const response = await fetch("https://api.openai.com/v1/chat/completions", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${API_KEY}`,
+//           },
+//           body: JSON.stringify(data),
+//         });
+
+//         // Process the response and extract content for this chunk
+//         const responseData = await response.json();
+//         const generatedContent = responseData.generatedContent; // Adjust based on API response structure
+
+//         // Extraction of the title and response content
+//         const titleRegex = /!(.*?)!/;
+//         const titleMatch = titleRegex.exec(generatedContent);
+//         const title = titleMatch ? titleMatch[1] : "";
+
+//         // Separating title from the main content
+//         const contentWithoutTitle = generatedContent.replace(titleRegex, "");
+
+//         // Constructing the chunk with title and response content
+//         const chunk = {
+//           title,
+//           content: contentWithoutTitle,
+//         };
+
+//         // Push the generated chunk to the storyChunks array
+//         storyChunks.push(chunk);
+
+//         // Update totalTokens with tokensInThisChunk
+//         totalTokens += tokensInThisChunk;
+//       } catch (error) {
+//         // Handle errors from API fetch
+//         throw new Error("Error fetching story content");
+//       }
+//     }
+
+//     // Return the generated story chunks
+//     return storyChunks;
+//   };
+
+//   try {
+//     // Generate the story chunks
+//     const story = await generateNextChunk();
+
+//     // Respond with the generated story chunks or handle as needed
+//     res.json({ result: "Story generated successfully", story });
+//   } catch (error) {
+//     // Handle errors if any
+//     res.status(500).json({ result: "Error generating story", error: error.message });
+//   }
+// });
