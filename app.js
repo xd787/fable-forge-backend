45c3e0a11ffd301d4fa53457ba7f1bb98721a411
Alienor <alienordms@gmail.com>
2023-12-07 10:44:43 +0100

    port

--- app.js
+++ app.js
@@ -6,36 +6,60 @@ const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const http = require('http');
-const WebSocket = require('ws');
+const http = require('http')
+const WebSocket = require ('ws')
+const fs = require('fs');
+const https = require('https');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const storiesRouter = require('./routes/stories');
+const storiesRouter = require('./routes/stories')
+
 
 var app = express();
 const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 
-// Créer un serveur HTTP
-const server = http.createServer(app);
+  // Créer un serveur HTTP
+  // const server = http.createServer(app);
+  
+  // Initialize WebSocket Server over HTTPS
+  //const wss = new WebSocket.Server({server});
+  
+  // Your WebSocket API initialization function
+  // const { initializeWebSocket } = require('./routes/api.js');
+  // initializeWebSocket(wss);
+  
+  // server.listen(8001, () => {
+  //   console.log('Secure WebSocket server running on port 8001');
+  // });
 
-// Initialize WebSocket Server over HTTP
-const wss = new WebSocket.Server({ server });
 
-// Your WebSocket API initialization function
+//   // Read your SSL certificate and key
+  // Load the .pfx file
+  const pfxPath = path.join(__dirname, 'certifs', 'certificat-fable-forge.pfx');
+  const pfxFile = fs.readFileSync(pfxPath);
+
+  // Extract the key and certificate from the .pfx file
+  const credentials = {
+  pfx: pfxFile,
+  passphrase: 'fable-forge', // Add passphrase if the .pfx file is encrypted
+  };
+
+// // Create an HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// // Initialize WebSocket Server over HTTPS
+const wss = new WebSocket.Server({ server: httpsServer });
+
+// // Your WebSocket API initialization function
 const { initializeWebSocket } = require('./routes/api.js');
 initializeWebSocket(wss);
 
-// Utiliser le port fourni par l'environnement, sinon 8001 par défaut
-const PORT = process.env.PORT || 8001;
-
-// Log pour vérifier la valeur du PORT
-console.log(`Valeur de la variable d'environnement PORT: ${process.env.PORT}`);
-console.log(`Le serveur va écouter sur le port: ${PORT}`);
-
-server.listen(PORT, () => {
+// // Start the HTTPS server on port 443 (standard HTTPS port)
+const PORT = 443; // Port 443 is the standard HTTPS port
+httpsServer.listen(PORT, () => {
   console.log(`Secure WebSocket server running on port ${PORT}`);
 });
 
@@ -47,6 +71,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/stories', storiesRouter);
+app.use('/stories', storiesRouter)
 
 module.exports = app;
