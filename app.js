bfec422e13a70280b8c5a1d9e58fde5c2a011e86
Alienor <alienordms@gmail.com>
2023-12-07 16:40:01 +0100

    websocket io

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const http = require('http')
 const WebSocket = require ('ws')
+const socketIO = require("socket.io");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,19 +23,35 @@ app.use(cors());
 // Créer un serveur HTTP
 const server = http.createServer(app);
   
-// Initialize WebSocket Server over HTTPS
-const wss = new WebSocket.Server({server});
+// // Initialize WebSocket Server over HTTPS
+// const wss = new WebSocket.Server({server});
   
-// Your WebSocket API initialization function
-const { initializeWebSocket } = require('./routes/api.js');
-initializeWebSocket(wss);
+// // Your WebSocket API initialization function
+// const { initializeWebSocket } = require('./routes/api.js');
+// initializeWebSocket(wss);
   
+
+// SOCKET IO 
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const {socketInitiation} = require ('./routes/socket.js')
+socketInitiation(io)
+
+
+
 // Déterminer le port à utiliser
 const PORT = process.env.PORT || 8001;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
 app.use(logger('dev'));
