f6e9c90b8088f7d20523ad139e4c9e1a54b39c8e
Jimmy Bellavia <bellavia.jimmy@gmail.com>
2023-12-07 07:53:26 +0100

    jim certif

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// const http = require('http')
-const WebSocket = require ('ws')
-const fs = require('fs');
 const https = require('https');
+const fs = require('fs');
+const WebSocket = require ('ws')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,31 +20,16 @@ const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 
-  // Créer un serveur HTTP
-  // const server = http.createServer(app);
-  
-  // Initialize WebSocket Server over HTTPS
-  // const wss = new WebSocket.Server({server});
-  
-  // Your WebSocket API initialization function
-  // const { initializeWebSocket } = require('./routes/api.js');
-  // initializeWebSocket(wss);
-  
-  // server.listen(8001, () => {
-  //   console.log('Secure WebSocket server running on port 8001');
-  // });
+// Read your SSL certificate and key
+// Load the .pfx file
+const pfxPath = path.join(__dirname, 'certifs', 'certificat-fable-forge.pfx');
+const pfxFile = fs.readFileSync(pfxPath);
 
-
-//   // Read your SSL certificate and key
-  // Load the .pfx file
-  const pfxPath = path.join(__dirname, 'certifs', 'certificat-fable-forge.pfx');
-  const pfxFile = fs.readFileSync(pfxPath);
-
-  // Extract the key and certificate from the .pfx file
-  const credentials = {
-  pfx: pfxFile,
-  passphrase: 'fable-forge', // Add passphrase if the .pfx file is encrypted
-  };
+// Extract the key and certificate from the .pfx file
+const credentials = {
+pfx: pfxFile,
+passphrase: 'fable-forge', // Add passphrase if the .pfx file is encrypted
+};
 
 // Create an HTTPS server
 const httpsServer = https.createServer(credentials, app);
@@ -53,20 +37,9 @@ const httpsServer = https.createServer(credentials, app);
 // Initialize WebSocket Server over HTTPS
 const wss = new WebSocket.Server({ server: httpsServer });
 
-  // Initialize WebSocket Server over HTTPS
-  // const wss = new WebSocket.Server({server});
-  
-  // Your WebSocket API initialization function
-  const { initializeWebSocket } = require('./routes/api.js');
-  initializeWebSocket(wss);
-  
-  // server.listen(8001, () => {
-  //   console.log('Secure WebSocket server running on port 8001');
-  // });
-
-// // Your WebSocket API initialization function
-// const { initializeWebSocket } = require('./routes/api.js');
-// initializeWebSocket(wss);
+// Your WebSocket API initialization function
+const { initializeWebSocket } = require('./routes/api.js');
+initializeWebSocket(wss);
 
 // // Start the HTTPS server on port 443 (standard HTTPS port)
 const PORT = process.env.PORT || 443; // Port 443 is the standard HTTPS port
